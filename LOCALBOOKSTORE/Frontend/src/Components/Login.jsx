700b500e40575767a28b08b8cb85c49084a07115
Ali12345678900000000 <f2022065286@umt.edu.pk>
2024-10-16 07:14:31 +0000

    get username and password in login pg

--- LOCALBOOKSTORE/Frontend/src/Components/Login.jsx
+++ LOCALBOOKSTORE/Frontend/src/Components/Login.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
 import Signup from './Signup';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+    // console.log(watch("example")); // watch input value by passing the name of it
+
+
+
     return (
         <div>
             {/* You can open the modal using document.getElementById('ID').showModal() method */}
             <dialog id="my_modal_3" className="modal">
                 <div className="modal-box">
-                    <form method="dialog">
+                    <form onSubmit={handleSubmit(onSubmit)} method="dialog">
                         {/* if there is a button in form, it will close the modal */}
+
                         <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+
+                        <h3 className="font-bold text-3xl">LogIn</h3>
+
+                        <div className='mt-10 space-y-2'>
+                            <label>Email</label>
+                            <br />
+                            <input className='input input-bordered' {...register("email", { required: true })} type="text" placeholder='Enter email here' />
+                            {errors.email && <span className='text-sm text-red-500'>This field is required</span>}
+                        </div>
+                        {/* password */}
+                        <div className='mt-3 space-y-2'>
+                            <label>Password</label>
+                            <br />
+                            <input className='input input-bordered' {...register("password", { required: true })} type="password" placeholder='Enter password here' />
+                            {errors.password && <span className='text-sm text-red-500'>This field is required</span>}
+                        </div>
+                        <div className='space-y-3'>
+                            <button className='btn btn-secondary mt-4' type='submit'>Login</button>
+                            <p>Create account <a className="link link-secondary" onClick={() => { document.getElementById("my_modal_3").close(); document.getElementById("my_modal_4").showModal() }} >SignUp</a></p>
+                        </div>
                     </form>
-
-                    <h3 className="font-bold text-3xl">LogIn</h3>
-
-                    <div className='mt-10 space-y-2'>
-                        <label>Email</label>
-                        <br />
-                        <input className='input input-bordered' type="text" placeholder='Enter email here' />
-                    </div>
-                    {/* password */}
-                    <div className='mt-3 space-y-2'>
-                        <label>Password</label>
-                        <br />
-                        <input className='input input-bordered' type="password" placeholder='Enter password here' />
-                    </div>
-                    <div className='space-y-3'>
-                        <button className='btn btn-secondary mt-4'>Login</button>
-                        <p>Create account <a className="link link-secondary" onClick={()=>{document.getElementById("my_modal_3").close(); document.getElementById("my_modal_4").showModal()}} >SignUp</a></p>
-                    </div>
-                    <Signup/>
+                    <Signup />
                 </div>
             </dialog>
         </div>
